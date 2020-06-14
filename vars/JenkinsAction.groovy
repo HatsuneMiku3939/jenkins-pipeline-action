7863dcce5a2349eea7052c67ebd55556f8587741
filepang <filepang@gmail.com>
2020-06-14 15:42:25 +0900

    Update

--- vars/JenkinsAction.groovy
+++ vars/JenkinsAction.groovy
@@ -1,13 +1,6 @@
+import java.security.*
+
 def call(def options) {
-	def rand1 = env['BUILD_TAG'].digest('SHA-1').substring(0, 12)
-	def dindNetwork = "dind-${rand1}"
-
-	def dindImg = 'docker:18.09.9-dind'
-	def dockerImg = "docker:18.09.9"
-	def dindArgs = ""
-	dindArgs += "--privileged "
-	dindArgs += "--network ${dindNetwork} "
-
 	// pull actionImage
 	sh "docker pull ${options.action}"
 	def actionEntrypoint = sh(returnStdout: true,
@@ -17,21 +10,18 @@ def call(def options) {
 
 	// execute action
 	stage("${options.name}") {
-		WithDockerNetwork(dindNetwork) {
-			docker.image(dindImg).withRun(dindArgs) { c ->
-				def actionRunArgs = ""
-				actionRunArgs += "--network ${dindNetwork} "
-				actionRunArgs += "-e DOCKER_HOST=tcp://${c.id.substring(0, 12)}:2375 "
-				actionRunArgs += "--entrypoint='' "
+		// override entrypoint, Jenkins need it.
+		def actionRunArgs = "--entrypoint='' "
 
-				def entrypointArgs = ""
-				actionArgs.split(",").each { argName ->
-					entrypointArgs += "\"${options.args[argName]}\""
-				}
-				withDockerContainer(image: options.action, args: actionRunArgs) {
-					sh "${actionEntrypoint} ${entrypointArgs}"
-				}
-			}
+		// action entrypoint
+		def entrypointArgs = ""
+		actionArgs.split(",").each { argName ->
+			entrypointArgs += "\"${options.args[argName]}\""
+		}
+
+		// run action
+		withDockerContainer(image: options.action, args: actionRunArgs) {
+			sh "${actionEntrypoint} ${entrypointArgs}"
 		}
 	}
 }
