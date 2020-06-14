9bc6543c74714db92c0503b0cf19a539c5c71020
filepang <filepang@gmail.com>
2020-06-14 15:57:42 +0900

    Update

--- vars/JenkinsAction.groovy
+++ vars/JenkinsAction.groovy
@@ -3,25 +3,18 @@ import java.security.*
 def call(def options) {
 	// pull actionImage
 	sh "docker pull ${options.action}"
-	def actionEntrypoint = sh(returnStdout: true,
-		script: "docker inspect ${options.action} -f '{{ .Config.Labels.ACTION_ENTRYPOINT }}'" ).trim()
 	def actionArgs = sh(returnStdout: true,
 		script: "docker inspect ${options.action} -f '{{ .Config.Labels.ACTION_ARGS }}'" ).trim()
 
 	// execute action
 	stage("${options.name}") {
-		// override entrypoint, Jenkins need it.
-		def actionRunArgs = "--entrypoint='' "
-
-		// action entrypoint
+		// actions args
 		def entrypointArgs = ""
 		actionArgs.split(",").each { argName ->
 			entrypointArgs += "\"${options.args[argName]}\""
 		}
 
 		// run action
-		withDockerContainer(image: options.action, args: actionRunArgs) {
-			sh "${actionEntrypoint} ${entrypointArgs}"
-		}
+		sh "docker run --rm ${options.action} ${entrypointArgs}"
 	}
 }
